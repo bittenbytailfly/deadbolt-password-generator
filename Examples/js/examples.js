60de40245ed17c6d240d8fe976b2f70a6b7eabd2
Ed Carter <bittenbytailfly@gmail.com>
2013-01-28 08:32:39 -0500

    Finished with the example usage file

--- Examples/js/examples.js
+++ Examples/js/examples.js
@@ -8,7 +8,16 @@ function deadboltExample() {
     self.short = ko.observable('');
     
     self.generatePassword = function() {
-        self.simple(encodePassword(self.passPhrase, '0000', false, false, 15));
+        if (self.phrase().length > 0) {
+            self.simple(encodePassword(self.phrase(), '0000', false, false, 15));
+            self.withPin(encodePassword(self.phrase(), '1234', false, false, 15));
+            self.withSpecial(encodePassword(self.phrase(), '1234', true, false, 15));
+            self.caseSensitive(encodePassword(self.phrase(), '1234', true, true, 15));
+            self.short(encodePassword(self.phrase(), '1234', true, true, 8));
+        }
+        else {
+            alert('You must enter a pass phrase');
+        }
     }
 };
 
